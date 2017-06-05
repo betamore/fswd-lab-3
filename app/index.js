faddf487e185cd30c7e0ab5f0fa246cd50083a83
David Raynes <rayners@gmail.com>
2017-06-05 17:02:46 -0400

    Turn off/nix browser js code

--- app/index.js
+++ app/index.js
@@ -3,32 +3,27 @@
 // require('bootstrap/css/bootstrap-theme.css!');
 
 require('bootstrap-webpack');
-var $ = require('jquery');
+// var $ = require('jquery');
 
-// Make jquery available in the console
-// window.$ = $;
-
-$('body').show();
-
-$(function() {
-  $('button#add').click(function(e) {
-    // Make sure the form does not get submitted
-    e.preventDefault();
-
-    // extract the current value of the todo
-    var newTodo = $('input[name=todo]').val();
-
-    // create a completely new (unattached)
-    // list item element (<li>)
-    var newListElement = $('<li></li>');
-
-    // Set its text
-    newListElement.text(newTodo);
-
-    // And add it to the end of the list
-    $('ul').append(newListElement);
-
-    // Then clear out the input element
-    $('input[name=todo]').val("");
-  });
-});
+// $(function() {
+//   $('button#add').click(function(e) {
+//     // Make sure the form does not get submitted
+//     e.preventDefault();
+//
+//     // extract the current value of the todo
+//     var newTodo = $('input[name=todo]').val();
+//
+//     // create a completely new (unattached)
+//     // list item element (<li>)
+//     var newListElement = $('<li></li>');
+//
+//     // Set its text
+//     newListElement.text(newTodo);
+//
+//     // And add it to the end of the list
+//     $('ul').append(newListElement);
+//
+//     // Then clear out the input element
+//     $('input[name=todo]').val("");
+//   });
+// });
