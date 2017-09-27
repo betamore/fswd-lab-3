c53c9531b333d66517086c27ee2f544e86761e8d
David Raynes <rayners@gmail.com>
2017-09-27 19:36:18 -0400

    more in-class demos

--- models/task.js
+++ models/task.js
@@ -7,10 +7,58 @@ module.exports = (sequelize, DataTypes) => {
         validate: {
             notEmpty: true
         }
+    },
+    completedAt: DataTypes.DATE
+  }, {
+    scopes: {
+        completed: {
+            where: {
+                completedAt: {
+                    $ne: null
+                }
+            }
+        },
+        incomplete: {
+            where: {
+                completedAt: null
+            }
+        },
+        last7Days: function() {
+            return {
+                where: {
+                    completedAt: {
+                        $lt: (new Date()) - (7 * 24 * 60 * 60 * 1000)
+                    }
+                }
+            };
+        },
+        lastXDays: function(x) {
+            return {
+                where: {
+                    completedAt: {
+                        $lt: (new Date()) - (x * 24 * 60 * 60 * 1000)
+                    }
+                }
+            };
+        }
     }
   });
+
+  Task.findLastXDays = function(x) {
+      return Task.scope({ method: ['lastXDays', x]}).findAll();
+  }
+
   Task.associate = function(models) {
     Task.belongsTo(models.User);
   };
+
+  Task.prototype.markCompleted = function() {
+      return this.update({ completedAt: new Date() });
+  };
+
+  Task.prototype.isCompleted = function() {
+      return !!this.completedAt;
+  };
+
   return Task;
 };
