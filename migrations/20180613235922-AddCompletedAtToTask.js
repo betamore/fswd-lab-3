00b2cd92557b1166d94e64ed1f7a475805c1b868
David Raynes <rayners@gmail.com>
2018-06-18 20:22:36 -0400

    In class models and sessions demo

--- migrations/20180613235922-AddCompletedAtToTask.js
+++ migrations/20180613235922-AddCompletedAtToTask.js
@@ -3,23 +3,9 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('Tasks', 'completedAt', Sequelize.DATE);
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.removeColumn('Tasks', 'completedAt');
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
   }
 };
--- models/task.js
+++ models/task.js
@@ -3,9 +3,33 @@ module.exports = (sequelize, DataTypes) => {
   var Task = sequelize.define('Task', {
     name: DataTypes.STRING,
     completedAt: DataTypes.DATE
-  }, {});
+  }, {
+    scopes: {
+      complete: {
+        where: {
+          completedAt: {
+            $ne: null
+          }
+        }
+      },
+      incomplete: {
+        where: {
+          completedAt: null
+        }
+      }
+    }
+  });
   Task.associate = function(models) {
     // associations can be defined here
   };
+
+  Task.prototype.isComplete = function() {
+    return !!this.completedAt;
+  };
+
+  Task.prototype.markComplete = function() {
+    return this.update({ completedAt: new Date() });
+  }
+
   return Task;
 };
